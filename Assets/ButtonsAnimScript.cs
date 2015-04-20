b88813e4dfa053e461c11de5fe4e5a7424dc2849
Mikozouzou <a.adeline@rubika-edu.com>
2015-04-21 00:17:49 +0200

    Ajout de l'animation du menu de pose de tour
    Début d'animation du menu de pose de tour
    --> Ne fonctionne qu'au moment de la pose

--- Assets/ButtonsAnimScript.cs
+++ Assets/ButtonsAnimScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ButtonsAnimScript : MonoBehaviour {
 
+    public bool activeSelf;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,11 +13,12 @@ public class ButtonsAnimScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-	    
+	    if(this.activeSelf == true)
+        {
+            Debug.Log(activeSelf);
+        }
 	}
 
-    void StartAnim ()
-    {
 
-    }
+
 }
