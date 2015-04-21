99d90d8071009a2616404bd1448260be8d64ccc3
Mikozouzou <antonin.adeline@gmail.com>
2015-04-21 10:11:48 +0200

    Fix du Spawn des vagues
    Réparation du système de spawn des vagues

--- Assets/Level Design/Spawn.cs
+++ Assets/Level Design/Spawn.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 using System.Collections;
+using System.Collections.Generic;
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                        //
@@ -12,15 +13,26 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour
 {
+    //Informations concernant les vagues à générer
+    public List<GameObject> vague1;
+    public List<GameObject> vague2;
+    public List<GameObject> vague3;
+    public List<GameObject> vague4;
+    public List<GameObject> vague5;
+    public List<GameObject> vague6;
+
+    public List<List<GameObject>> vagues;
 
     //Temps pour le spawn
-
     public float interval;
     public float frequenceSpawnDifferent;
     private float timeLeft;
 
     //Numéro de l'ennemi généré
     private int numero_ennemi;
+
+    //Indique si une vague est terminée
+    private bool vagueTerminee = false;
 
     // L'objet que l'on doit faire spawner
     public GameObject ennemy;
@@ -32,43 +44,65 @@ public class Spawn : MonoBehaviour
     void Start()
     {
         //Initialisation des variables
+        vagues = new List<List<GameObject>>(new List<GameObject>[]{vague1, vague2, vague3, vague4, vague5, vague6});
         timeLeft = 0;
         numero_ennemi = 0;
     }
 
     void Update()
     {
-
         // time to spawn the next one?
         timeLeft -= Time.deltaTime;
 
-        //Simple timer, a améliorer
-        if (timeLeft <= 0.0f)
+        //Fréquence de spawn d'un ennemi
+        if (timeLeft <= 0.0f && vagueTerminee == false)
         {
             timeLeft = interval;
-
-            GameObject spawner = null;
-            // spawn
-            if (numero_ennemi >= frequenceSpawnDifferent)
-            {
-                spawner = (GameObject)Instantiate(boostaure, transform.position, Quaternion.identity);
-                numero_ennemi = 0;
-            }
-            else
-            {
-                spawner = (GameObject)Instantiate(ennemy, transform.position, Quaternion.identity);
-            }
-            
-
-            // get access to the navmesh agent component
-            NavMeshAgent nav_ennemy = spawner.GetComponent<NavMeshAgent>();
-
-            nav_ennemy.destination = destination.position;
-
-            // on incrémente le nombre d'ennemis créés
-            numero_ennemi++;
+            SpawnEnnemi();
         }
 
+        //Déclenche la vague suivante si la vague précédente a fini d'être envoyée en appuyant sur Espace en attendant que les ennemis puissent être tués
+        if (Input.GetKeyDown("space") && vagueTerminee)
+        {
+            vagueTerminee = false;
+        }
+    }
+
+    void SpawnEnnemi()
+    {
+        GameObject spawner = null;
+        
+       /*f (numero_ennemi >= frequenceSpawnDifferent)
+        {
+            spawner = (GameObject)Instantiate(boostaure, transform.position, Quaternion.identity);
+            numero_ennemi = 0;
+        }
+        else
+        {
+            spawner = (GameObject)Instantiate(ennemy, transform.position, Quaternion.identity);
+        }*/
+
+        if(vagues[0].Count > 0)
+        {
+            spawner = (GameObject)Instantiate(vagues[0][0], transform.position, Quaternion.identity);
+            vagues[0].RemoveAt(0);
+            if (vagues[0].Count == 0)
+            {
+                vagues.RemoveAt(0);
+                vagueTerminee = true;
+            }
+        }
+
+
+        //On attribue le navmesh à l'ennemi créé
+        NavMeshAgent nav_ennemy = spawner.GetComponent<NavMeshAgent>();
+        //On attribue la destination à l'ennemi créé
+        nav_ennemy.destination = destination.position;
+
+        // numero_enemi%4 --> index de l'ennemi 
+
+        // on incrémente le nombre d'ennemis créés
+        numero_ennemi++;
     }
 
 }
