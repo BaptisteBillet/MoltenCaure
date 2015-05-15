11cda7e31ff3ce8337e416e4e1c1e1fb35086f11
Baptiste <b.billet@rubika-edu.com>
2015-05-15 21:17:44 +0200

    Add amelioration

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -11,6 +11,7 @@ public class Ennemy : MonoBehaviour {
 
     //Vitesse de l'ennemi
     public float speed;
+	private float base_speed;
     //Vie de l'ennemi
     public float vie;
 
@@ -21,10 +22,16 @@ public class Ennemy : MonoBehaviour {
 
 	public NavMeshAgent nav_ennemy;
 
-
+	public bool IsPoison;
+	public int poison_time;
+	public bool IsGel;
+	public int gel_time;
+	public bool IsRadia;
+	public int radia_time;
 
 	public virtual void Start()
 	{
+		base_speed = speed;
 		//On attribue le navmesh à l'ennemi créé
 		nav_ennemy = this.gameObject.GetComponent<NavMeshAgent>();
 	}
@@ -56,6 +63,68 @@ public class Ennemy : MonoBehaviour {
 			nav_ennemy.destination = Artefact_Script.instance.artefact.transform.position;
 		}
 
+	}
+
+	public IEnumerator Poison(int degats, int time)
+	{
+		if(!IsPoison)
+		{
+			IsPoison = true;
+			poison_time = 0;
+			while (poison_time < time)
+			{
+				vie -= degats;
+				yield return new WaitForSeconds(1);
+				poison_time++;
+			}
+			IsPoison = false;
+		}
+		else
+		{
+			poison_time = 0;
+		}
+	}
+
+	public IEnumerator Gel(int slow, int time)
+	{
+		if(!IsGel)
+		{
+			IsGel = true;
+			gel_time = 0;
+			while(gel_time!=time)
+			{
+				speed -= slow;
+				yield return new WaitForSeconds(1);
+				gel_time++;
+			}
+			speed = base_speed;
+			IsGel = false;
+		}
+		else
+		{
+			gel_time = 0;
+		}
+		
+	}
+	public IEnumerator Radia(int time)
+	{
+		if (!IsRadia)
+		{
+			IsRadia = true;
+			radia_time = 0;
+			while (radia_time != time)
+			{
+				IsRadia = true;
+				yield return new WaitForSeconds(1);
+				radia_time = 0;
+			}
+			IsRadia = false;
+		}
+		else
+		{
+			radia_time = 0;
+		}
 
 	}
+
 }
